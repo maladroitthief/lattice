db720d9d0800406b4caceb3bfc800f38d3eaef76
Ian Weller <maladroitthief@gmail.com>
2024-04-06 00:09:17 -0400

    feat: A* Search
    - Adds the A* path finding algorithm
    - Modifies spatial grid to be weighted

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/maladroitthief/lattice
 go 1.22.0
 
 require (
-	github.com/maladroitthief/caravan v1.1.0
-	github.com/maladroitthief/mosaic v1.0.0
+	github.com/maladroitthief/caravan v1.3.0
+	github.com/maladroitthief/mosaic v1.1.0
 )
--- spatial_grid_test.go
+++ spatial_grid_test.go
@@ -65,88 +65,238 @@ func Test_spatial_grid_Insert(t *testing.T) {
 }
 
 func Test_spatial_grid_Search(t *testing.T) {
-	type fields struct {
-		x    int
-		y    int
-		size float64
-	}
-	type params struct {
-		item   int
+	type item struct {
+		value  int
 		bounds mosaic.Rectangle
 	}
-	type wants struct {
+	type fields struct {
+		x     int
+		y     int
+		size  float64
+		items []item
+	}
+	type params struct {
 		x     float64
 		y     float64
 		depth int
+	}
+	type want struct {
 		items []int
 	}
 	tests := []struct {
 		name   string
 		fields fields
-		params []params
-		wants  []wants
+		params params
+		want   want
 	}{
 		{
-			name:   "shallow search",
-			fields: fields{x: 4, y: 4, size: 8},
-			params: []params{
-				{item: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
-				{item: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
-				{item: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
-				{item: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
+			name: "shallow search",
+			fields: fields{
+				x:    4,
+				y:    4,
+				size: 8,
+				items: []item{
+					{value: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
+					{value: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
+					{value: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
+					{value: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
+				},
 			},
-			wants: []wants{
-				{x: 20, y: 20, depth: 1, items: []int{3}},
+			params: params{
+				x:     20,
+				y:     20,
+				depth: 1,
 			},
-		},
-		{
-			name:   "wide search",
-			fields: fields{x: 32, y: 32, size: 8},
-			params: []params{
-				{item: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
-				{item: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
-				{item: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
-				{item: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
-			},
-			wants: []wants{
-				{x: 20, y: 20, depth: 5, items: []int{1, 2, 3, 4}},
+			want: want{
+				items: []int{3},
 			},
 		},
 		{
-			name:   "wide search",
-			fields: fields{x: 32, y: 32, size: 8},
-			params: []params{
-				{item: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
-				{item: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
-				{item: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
-				{item: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
+			name: "wide search",
+			fields: fields{
+				x:    32,
+				y:    32,
+				size: 8,
+				items: []item{
+					{value: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
+					{value: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
+					{value: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
+					{value: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
+				},
 			},
-			wants: []wants{
-				{x: 0, y: 0, depth: 2, items: []int{1, 2}},
+			params: params{
+				x:     20,
+				y:     20,
+				depth: 5,
+			},
+			want: want{
+				items: []int{1, 2, 3, 4},
+			},
+		},
+		{
+			name: "medium search",
+			fields: fields{
+				x:    32,
+				y:    32,
+				size: 8,
+				items: []item{
+					{value: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 4, Y: 4}, 2, 2)},
+					{value: 2, bounds: mosaic.NewRectangle(mosaic.Vector{X: 12, Y: 12}, 2, 2)},
+					{value: 3, bounds: mosaic.NewRectangle(mosaic.Vector{X: 20, Y: 20}, 2, 2)},
+					{value: 4, bounds: mosaic.NewRectangle(mosaic.Vector{X: 28, Y: 28}, 2, 2)},
+				},
+			},
+			params: params{
+				x:     0,
+				y:     0,
+				depth: 2,
+			},
+			want: want{
+				items: []int{1, 2},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			sg := lattice.NewSpatialGrid[int](tt.fields.x, tt.fields.y, tt.fields.size)
-			for _, param := range tt.params {
-				sg.Insert(param.item, param.bounds)
+			for _, item := range tt.fields.items {
+				sg.Insert(item.value, item.bounds)
 			}
 
-			for _, want := range tt.wants {
-				got := []int{}
-				check := func(items []int) error {
-					got = append(got, items...)
-					return nil
-				}
+			got := []int{}
+			want := tt.want.items
+			check := func(items []int) error {
+				got = append(got, items...)
+				return nil
+			}
 
-				sg.Search(want.x, want.y, want.depth, check)
-				slices.Sort(want.items)
-				slices.Sort(got)
+			sg.Search(tt.params.x, tt.params.y, tt.params.depth, check)
+			slices.Sort(want)
+			slices.Sort(got)
 
-				if !slices.Equal(want.items, got) {
-					t.Error(fmt.Errorf("spatialGrid.Search() want: %+v, got: %+v\n", want.items, got))
-				}
+			if !slices.Equal(want, got) {
+				t.Error(fmt.Errorf("spatialGrid.Search() want: %+v, got: %+v\n", want, got))
+			}
+		})
+	}
+}
+
+func Test_spatial_grid_WeightedSearch(t *testing.T) {
+	type item struct {
+		value  int
+		bounds mosaic.Rectangle
+	}
+	type fields struct {
+		x     int
+		y     int
+		size  float64
+		items []item
+	}
+	type params struct {
+		start mosaic.Vector
+		end   mosaic.Vector
+	}
+	type want struct {
+		path []mosaic.Vector
+		err  error
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		params params
+		want   want
+	}{
+		{
+			name: "simple path",
+			fields: fields{
+				x:     4,
+				y:     4,
+				size:  10,
+				items: []item{},
+			},
+			params: params{
+				start: mosaic.NewVector(5, 5),
+				end:   mosaic.NewVector(10, 5),
+			},
+			want: want{
+				path: []mosaic.Vector{
+					{X: 5, Y: 5},
+					{X: 15, Y: 5},
+				},
+				err: nil,
+			},
+		},
+		{
+			name: "long path",
+			fields: fields{
+				x:     4,
+				y:     4,
+				size:  10,
+				items: []item{},
+			},
+			params: params{
+				start: mosaic.NewVector(5, 5),
+				end:   mosaic.NewVector(35, 5),
+			},
+			want: want{
+				path: []mosaic.Vector{
+					{X: 5, Y: 5},
+					{X: 15, Y: 5},
+					{X: 25, Y: 5},
+					{X: 35, Y: 5},
+				},
+				err: nil,
+			},
+		},
+		{
+			name: "long path with obstacles",
+			fields: fields{
+				x:    4,
+				y:    4,
+				size: 10,
+				items: []item{
+					{value: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 15, Y: 5}, 8, 8)},
+					{value: 1, bounds: mosaic.NewRectangle(mosaic.Vector{X: 25, Y: 5}, 8, 8)},
+				},
+			},
+			params: params{
+				start: mosaic.NewVector(5, 5),
+				end:   mosaic.NewVector(35, 5),
+			},
+			want: want{
+				path: []mosaic.Vector{
+					{X: 5, Y: 5},
+					{X: 5, Y: 15},
+					{X: 15, Y: 15},
+					{X: 25, Y: 15},
+					{X: 35, Y: 15},
+					{X: 35, Y: 5},
+				},
+				err: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sg := lattice.NewSpatialGrid[int](tt.fields.x, tt.fields.y, tt.fields.size)
+			for _, item := range tt.fields.items {
+				sg.Insert(item.value, item.bounds)
+			}
+
+			got, err := sg.WeightedSearch(tt.params.start, tt.params.end)
+
+			if err != tt.want.err {
+				t.Error(
+					fmt.Errorf(
+						"spatialGrid.WeightedSearch() error. want error: %+v, got error: %+v\n",
+						tt.want.err,
+						err,
+					),
+				)
+			}
+
+			if !slices.Equal(tt.want.path, got) {
+				t.Error(fmt.Errorf("spatialGrid.WeightedSearch() want: %+v, got: %+v\n", tt.want.path, got))
 			}
 		})
 	}
